0b21612aeb60404b714ab7dfe35f2d5def3d8c65
ayham_bsmar.dev <ayhambsmar36@gmail.com>
2025-12-27 04:53:43 +0300

    Fix AppServiceProvider namespace and force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\support\Facades\URL;
+use Illuminate\Support\Facades\URL;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
